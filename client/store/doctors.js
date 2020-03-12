0b27d88865afcf5490729a9143b3d4744d277c3b
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-12 19:31:06 -0400

    working with real data from an API call

--- client/store/doctors.js
+++ client/store/doctors.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const doctorsApiKey = process.env.DOCTORS_API_KEY;
+const api_key = process.env.DOCTORS_API_KEY;
+
+const doctorsResource =
+  'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' +
+  api_key;
 
 const GET_DOCTORS = 'GET_DOCTORS';
 
@@ -9,7 +13,7 @@ const initialState = {}; // have a normalized store of doctors
 /*
  * ACTION CREATORS
  */
-const getDoctors = user => ({ type: GET_USER, getDoctors });
+const getDoctors = doctors => ({ type: GET_DOCTORS, doctors });
 
 /*
  * THUNK CREATORS
@@ -19,10 +23,22 @@ export const loadDoctors = () => async dispatch => {
   try {
     // make API call to better doctors
 
-    const res = await axios.get('/auth/me');
+    const res = await axios.get(doctorsResource);
     console.log(res);
-    dispatch(getUser(res.data || initialState));
+    dispatch(getDoctors(res.data || initialState));
   } catch (err) {
     console.error(err);
   }
 };
+
+/**
+ * REDUCER
+ */
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_DOCTORS:
+      return action.doctors;
+    default:
+      return state;
+  }
+}
