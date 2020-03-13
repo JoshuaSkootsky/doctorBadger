b8919476a517e6e64d43ba9fb37cd26c69a963b7
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-13 00:49:11 -0400

    don't run the tests on travis

--- server/db/models/practice.spec.js
+++ server/db/models/practice.spec.js
@@ -1,36 +1,38 @@
 const { Practice } = require('./');
 const { expect } = require('chai');
 
-const practice = new Practice();
+if (process.env.NODE_ENV !== 'production') {
+  const practice = new Practice();
 
-const readPractices = async () => {
-  const data = await practice.findAll();
-  // console.log(data);
-  return data;
-};
+  const readPractices = async () => {
+    const data = await practice.findAll();
+    // console.log(data);
+    return data;
+  };
 
-readPractices(); // prints to console
+  readPractices(); // prints to console
 
-describe('Practice model', () => {
-  // beforeEach(() => {
-  //   return db.sync({ force: true });
-  // });
+  describe('Practice model', () => {
+    // beforeEach(() => {
+    //   return db.sync({ force: true });
+    // });
 
-  describe('class methods', () => {
-    describe('finds all', () => {
-      let practices;
+    describe('class methods', () => {
+      describe('finds all', () => {
+        let practices;
 
-      beforeEach(async () => {
-        practices = await readPractices();
-      });
+        beforeEach(async () => {
+          practices = await readPractices();
+        });
 
-      it('returns data with a meta attribute', () => {
-        expect(typeof practices.meta === 'object').to.be.equal(true);
-      });
+        it('returns data with a meta attribute', () => {
+          expect(typeof practices.meta === 'object').to.be.equal(true);
+        });
 
-      it('returns data with a data array', () => {
-        expect(practices.data.length > 0).to.be.equal(true);
-      });
-    }); // end describe('correctPassword')
-  }); // end describe('instanceMethods')
-}); // end describe('User model')
+        it('returns data with a data array', () => {
+          expect(practices.data.length > 0).to.be.equal(true);
+        });
+      }); // end describe('correctPassword')
+    }); // end describe('instanceMethods')
+  }); // end describe('User model')
+}
