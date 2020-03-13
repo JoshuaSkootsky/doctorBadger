8cdf9ce129b6f8c5ba16ec6252aee6d92b8a7922
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-13 09:29:32 -0400

    trying to go live with this API

--- server/db/models/practice.js
+++ server/db/models/practice.js
@@ -1,19 +1,32 @@
 const Promise = require('bluebird');
 const fs = require('fs');
+const axios = require('axios');
 // I want to import Bluebird and return a promise?
 
 // represents a medical practice.
 // Currently mocking up making an API call by reading from file
 class Practice {
   // in the future, this will have process.env.NODE_ENV === test wrapped around the test data
-  findAll() {
+  async findAll() {
     // '../../../practices.json';
-    return new Promise(function(resolve, reject) {
-      const fileName = 'practices.json';
-      fs.readFile(fileName, (err, data) => {
-        err ? reject(err) : resolve(JSON.parse(data));
+    if (process.env.NODE_ENV === 'test') {
+      return new Promise(function(resolve, reject) {
+        const fileName = 'practices.json';
+        fs.readFile(fileName, (err, data) => {
+          err ? reject(err) : resolve(JSON.parse(data));
+        });
       });
-    });
+    }
+    // otherwise make a real API call
+    try {
+      const practices = await axios.get(
+        'https://api.betterdoctor.com/2016-03-01/practices?location=40.71%2C-74.00%2C100&user_location=40.71%2C-74.00&sort=distance-asc&skip=0&limit=12&user_key=' +
+          process.env.DOCTORS_API_KEY
+      );
+      return practices;
+    } catch (err) {
+      throw err;
+    }
   }
 }
 
